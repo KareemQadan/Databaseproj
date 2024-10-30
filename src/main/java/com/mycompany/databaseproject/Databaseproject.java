836bf8e76b20a17a2cd01179cf2ce7a31f94de46
KareemQadan <kareem.qadan7@gmail.com>
2024-10-30 19:03:38 +0200

    commit

---
+++ src/main/java/com/mycompany/databaseproject/Databaseproject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.databaseproject;
+
+/**
+ *
+ * @author X1
+ */
+public class Databaseproject {
+
+    public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(new Runnable() {
+             @Override
+             public void run() {
+                 new loginpage().setVisible(true);
+             }
+         });
+    }
+}
